e0e250f2df6dc151cb298df3d6a53e282341377c
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-08-25 12:35:57 -0500

    Redesign Student Profile (#10)
    * Show QR Code of ticket
    
    * Clean up Student Profile design

--- src/components/modals/studentProfile.js
+++ src/components/modals/studentProfile.js
@@ -1,5 +1,6 @@
 import React, { createRef } from "react";
 import { Header, Modal, Table, Button, Segment, Icon } from "semantic-ui-react";
+import QRCode from "react-qr-code";
 
 import StudentIdInput from "./studentIdInput";
 
@@ -76,7 +77,7 @@ class StudentModal extends React.Component {
               <Table.Header>
                 <Table.Row>
                   <Table.HeaderCell>Event</Table.HeaderCell>
-                  <Table.HeaderCell>Ticket ID</Table.HeaderCell>
+                  <Table.HeaderCell>Ticket</Table.HeaderCell>
                   <Table.HeaderCell />
                 </Table.Row>
               </Table.Header>
@@ -117,8 +118,28 @@ class StudentModal extends React.Component {
 
                   return (
                     <Table.Row key={val._id}>
-                      <Table.Cell disabled={hideTime <= now}>{slotName}</Table.Cell>
-                      <Table.Cell disabled={hideTime <= now}>{val._id}</Table.Cell>
+                      <Table.Cell disabled={hideTime <= now} collapsing>
+                        <div style={{display: 'flex', flexDirection: 'column'}}>
+                          <div style={{marginLeft: 'auto', marginRight: 'auto', marginBottom: 5, marginTop: 5}} >
+                            <h3>{
+                              slotName
+                            }</h3>
+                          </div>
+                          <div style={{marginLeft: 'auto', marginRight: 'auto', marginBottom: 5, marginTop: 5}} >
+                            <h3>{
+                              hideTime.toLocaleString("en-US")
+                            }</h3>
+                          </div>
+                        </div>
+                      </Table.Cell>
+                      <Table.Cell disabled={hideTime <= now} style={{display: 'flex'}}>
+                        <div style={{display: 'flex', flexDirection: 'column', marginLeft: 'auto', marginRight: 'auto'}} >
+                          
+                          <div style={{marginLeft: 'auto', marginRight: 'auto'}} >
+                            <QRCode value={val._id} />
+                          </div>
+                        </div>
+                      </Table.Cell>
                       <Table.Cell collapsing>
                         <Button
                           icon="delete"
