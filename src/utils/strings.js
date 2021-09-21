f7715aeffabaa0ae949ce4aa2c88228d87b536b2
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-09-21 00:04:43 -0400

    Fix date display

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,10 +1,66 @@
 export const displayDate = (date) => {
-    if(date === null || date === undefined)return "ERROR";
+    if (date === null || date === undefined) return "ERROR";
+
+    let now = new Date();
+
     let month = date.getMonth();
     let day = date.getDate();
     let hour = date.getHours();
     let min = date.getMinutes();
     let pm = hour > 11;
 
-    return (month + 1) + "/" + (day) + " " + (hour % 12) + ":" + ((min<10?'0':'') + min) + " " + (pm ? "PM" : "AM");
+    let finalStr = "";
+
+    if (now.getDate() !== day) { //If not today then show date
+        finalStr += (month + 1) + "/" + (day) + " ";
+    }
+
+    let hourStr = hour === 0 ? "12" : (hour % 12); //If midnight then show 12
+    finalStr += hourStr + ":" + ((min < 10 ? '0' : '') + min) + " " + (pm ? "PM" : "AM");
+    return finalStr;
+}
+
+export const displayDateRange = (date1, date2) => {
+    if (date1 === null || date1 === undefined) return "ERROR";
+    if (date2 === null || date2 === undefined) return "ERROR";
+
+    let now = new Date();
+
+    let month1 = date1.getMonth();
+    let day1 = date1.getDate();
+    let hour1 = date1.getHours();
+    let min1 = date1.getMinutes();
+    let pm1 = hour1 > 11;
+    let month2 = date2.getMonth();
+    let day2 = date2.getDate();
+    let hour2 = date2.getHours();
+    let min2 = date2.getMinutes();
+    let pm2 = hour2 > 11;
+
+    let finalStr = "";
+    if (month1 === month2 && day1 == day2) {
+        if (now.getDate() !== day1) { //Test if event is on a different day and display that date
+            finalStr += (month1 + 1) + "/" + (day1) + " ";
+        }
+    }
+    let hour1Str = hour1 === 0 ? "12" : (hour1 % 12); //If midnight then show 12
+    finalStr += hour1Str + ":" + ((min1 < 10 ? '0' : '') + min1);
+
+    if (pm1 != pm2) {
+        finalStr += " " + (pm1 ? "PM" : "AM");
+    }
+    finalStr += " - ";
+    if (month1 != month2) {
+        finalStr += (month2 + 1) + "/";
+    }
+    if (day1 != day2) { //If the second date is not on the same day display that date
+        if (month1 === month2) {
+            finalStr += (month2 + 1) + "/"
+        }
+        finalStr += (day2) + " ";
+    }
+
+    let hour2Str = hour2 === 0 ? "12" : (hour2 % 12); //If midnight then show 12
+    finalStr += hour2Str + ":" + ((min2 < 10 ? '0' : '') + min2) + " " + (pm2 ? "PM" : "AM");
+    return finalStr;
 }
